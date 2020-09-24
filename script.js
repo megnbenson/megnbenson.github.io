5888dab33f8633d3f9810b2d5eb1c84ca5ec9712
megnbenson <mbens003@gold.ac.uk>
2020-09-24 14:09:22 +0100

    changed homescreen with animationn

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init({
+  duration: 1200,
+})
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
