b087012f3904e29a6fa801430202ed601aa8edc0
megnbenson <mbens003@gold.ac.uk>
2024-02-08 18:51:35 +0000

    updates

--- sketch.js
+++ sketch.js
@@ -5,23 +5,39 @@ function setup() {
   fill(255,50);
   textSize(20);
   textAlign(CENTER)
-  text('circle size controlled by how high you tap the screen', innerWidth/4, innerHeight/2-100,innerWidth/2);
+  text('hi there, welcome to my portfolio, bit dusty in here apologies, have a rifle through my work, otherwise tap about a bit...', innerWidth/4, innerHeight/2-100,innerWidth/2);
 }
-
+let size = 3;
+let direction = 1;
 function draw() {
-  let time = millis() % 345
   if(millis() % 345 ==0){
     background(0);
+    direction = direction * -1;
   }
   // Get a gaussian random number w/ mean of 0 and standard deviation of 1.0
-  let val = randomGaussian();
+  // let val = randomGaussian();
 
-  let sd = 160;                  // Define a standard deviation
-  let mean = width/2;           // Define a mean value (middle of the screen along the x-axis)
-  let x = ( val * sd ) + mean;  // Scale the gaussian random number by standard deviation and mean
+  // let sd = 160;                  // Define a standard deviation
+  // let mean = width/2;           // Define a mean value (middle of the screen along the x-axis)
+  // let x = ( val * sd ) + mean;  // Scale the gaussian random number by standard deviation and mean
 
   noStroke();
   let mouseCol = map(mouseY,0,innerHeight,0,255)
+  colour = color(mouseCol/2, mouseCol, mouseCol,10)
   fill(mouseCol/2, mouseCol, mouseCol, 10);
-  ellipse(x, height/2, mouseCol/2, mouseCol/2);   // Draw an ellipse at our "normal" random location
+  // ellipse(x, height/2, mouseCol/2, mouseCol/2);   // Draw an ellipse at our "normal" random location
+
+  circles();
+}
+
+function circles() {
+  stroke(map(size,0,innerWidth+13, 100, 255).toFixed(0),map(size,innerWidth,0, 213, 26).toFixed(0),30);
+  fill(colour);
+  size+=direction;
+  ellipse(mouseX,mouseY,size,size);
+
+  if(size <2 || size > innerWidth/2){
+    direction = direction * -1;
+  }
+  return false;
 }
